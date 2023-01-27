babc29ccffab0de3fb5654070e77292cfb26cdb6
Julius Mwangi <cutejuliusmwash@gmail.com>
2023-01-27 21:00:17 +0300

    Edit functions2.c

--- functions2.c
+++ functions2.c
@@ -29,51 +29,6 @@ int print_string(va_list list)
 }
 
 /**
- * print_string2 - prints characters to stdout
- * @list: list of arguments
- * Return: count of characters printed
- */
-
-int print_string2(va_list list)
-{
-	int i, len, c;
-	char *str;
-
-	len = 0;
-	str = va_arg(list, char *);
-	if (str == NULL)
-		str = "(null)";
-	for (i = 0; str[i] != '\0'; i++)
-	{
-		c = str[i];
-		if ((c > 0 && c < 32) || c >= 127)
-		{
-		if (c < 16)
-		{
-			_write_char('\\');
-			_write_char('x');
-			_write_char('0');
-			len += print_heX2(c);
-			len += 3;
-		}
-		else
-		{
-			_write_char('\\');
-			_write_char('x');
-			len += print_heX2(c);
-			len += 2;
-		}
-		}
-		else
-		{
-			_write_char(str[i]);
-			len++;
-		}
-	}
-	return (len);
-}
-
-/**
  * print_percent - Prints a percent symbol
  * @list: list of arguments
  * Return: Will return the amount of characters printed.
