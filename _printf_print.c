44894f3f07c098cb4717300fab060da5376e2c1a
Julius Mwangi <cutejuliusmwash@gmail.com>
2023-01-29 14:32:08 +0300

    Fix errors on _printf_print.c

--- _printf_print.c
+++ _printf_print.c
@@ -49,9 +49,10 @@ int printfFlags(int *i, const char *format, conver_t f_list[], va_list arg_list)
 
 int parser_helper(int *i1, const char *format, conver_t f_list[], va_list arg_list)
 {
-	int printed_chars, i;
+	int printed_chars, i, r_val, j;
 
 	i = *(i1);
+	r_val = 0;
 	printed_chars = 0;
 	if (format[i] == '%') /*Checks for format specifiers*/
 	{
@@ -64,7 +65,7 @@ int parser_helper(int *i1, const char *format, conver_t f_list[], va_list arg_li
 				if (r_val == -1)
 					return (-1);
 				printed_chars += r_val;
-				break;£
+				break;
 			}
 		}
 		if (f_list[j].sym == NULL && format[i + 1] != ' ')
@@ -85,7 +86,7 @@ int parser_helper(int *i1, const char *format, conver_t f_list[], va_list arg_li
 		_write_char(format[i]); /*call the write function*/
 		printed_chars++;
 	}
-	return (printed chars);
+	return (printed_chars);
 }
 
 /**
@@ -97,16 +98,15 @@ int parser_helper(int *i1, const char *format, conver_t f_list[], va_list arg_li
  * Return: A total count of the characters printed.
  */
 
-
 int parser(const char *format, conver_t f_list[], va_list arg_list)
 {
-	int i, j, r_val, printed_chars;
+	int i, printed_chars;
 
 	printed_chars = 0;
 	for (i = 0; format[i] != '\0'; i++)/* Iterates through the main str*/
 	{
-		printed chars = printfFlags(&i, format, f_list[], arg_list);
-		printed_chars = parser_helper(&i, format, f_list[], arg_list);
+		printed_chars = printfFlags(&i, format, f_list, arg_list);
+		printed_chars = parser_helper(&i, format, f_list, arg_list);
 	}
 	return (printed_chars);
 }
